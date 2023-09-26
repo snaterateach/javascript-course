f2912a541a7679ca0b9d4cf87896939527318854
snatera <snatera.ve@gmail.com>
2023-09-25 22:48:37 -0300

    workshop

---
+++ workshop-resuelto/data/characters.js
@@ -0,0 +1,26 @@
+const characters = [
+    {
+        "_id": "1",
+        "name": "Pedro Picapiedra",
+        "description": "El personaje en sí es rudo, seguro de sí mismo, pero poco reflexivo. Siempre pasa por un error antes de aprenderlo y tiende a hacer todo lo posible para no enfrentar sus problemas.",
+        "image": "pedro.png"
+    },
+    {
+        "_id": "2",
+        "name": "Vilma Picapiedra",
+        "description": "La personalidad de Vilma es de una firme voluntad, como cabeza en su matrimonio, a menudo critica a Pedro por llevar a cabo sus diversos y extraños planes.",
+        "image": "vilma.png"
+    },
+    {
+        "_id": "3",
+        "name": "Pablo Marmol",
+        "description": "Las principales aficiones de Pablo son jugar a los bolos y las reuniones de la logia de los Búfalos Mojados, al igual que su amigo Pedro Picapiedra.",
+        "image": "pablo.png"
+    },
+    {
+        "_id": "4",
+        "name": "Betty Marmol",
+        "description": "Betty se convirtió en ama de casa, manteniendo su hogar con las herramientas prehistóricas y otras tecnologías de la época. Betty como Vilma disfruta de apoyar obras de caridad en Piedradura.",
+        "image": "betty.png"
+    }
+]
